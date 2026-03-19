f6c625e4c9ae0424342f9e3645d0ea838deef53a
jellie <talentforest0501@gmail.com>
2026-03-19 15:03:15 +0900

    feat: prettier, eslint,  husky, commitlint, lintstaged 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,19 @@
 const { defineConfig, globalIgnores } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 const prettier = require('eslint-config-prettier');
+const unusedImports = require('eslint-plugin-unused-imports');
 
 module.exports = defineConfig([
   globalIgnores(['dist/*']),
   expoConfig,
+  {
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
   prettier,
 ]);
